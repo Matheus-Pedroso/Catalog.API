0326df0222bf862e8c5e01dc453f92c6cb2429ca
Matheus Pedroso <matheus_pedroso2012@hotmail.com>
2025-06-17 23:37:38 -0300

    Update classe Category e Product com a modificador sealed, setters privados nas classes.
    Add classe exception validation
    Add Validation nas propriedades de category

--- Catalog.Domain/Entities/Category.cs
+++ Catalog.Domain/Entities/Category.cs
@@ -1,9 +1,36 @@
-﻿namespace Catalog.Domain.Entities;
+﻿using Catalog.Domain.Validations;
 
-public class Category
+namespace Catalog.Domain.Entities;
+
+public sealed class Category
 {
-    public int Id { get; set; }
-    public string Name { get; set; } 
+    public int Id { get; private set; }
+    public string Name { get; private set; } 
 
-    public ICollection<Product> Products { get; set; } 
+    public ICollection<Product> Products { get; set; }
+
+    public Category(string name)
+    {
+        ValidationDomain(name);
+    }
+
+    // Constructor for EF Core populate table
+    public Category(int id, string name)
+    {
+        DomainExceptionValidation.When(id < 0, "Invalid Id value");
+        Id = id;
+        ValidationDomain(name);
+    }
+
+
+    private void ValidationDomain(string name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrWhiteSpace(name),
+                "Invalid category name");
+
+        DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters");
+
+        Name = name;
+    }
 }
