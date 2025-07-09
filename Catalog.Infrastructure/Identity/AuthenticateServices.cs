e21c3c0979cd53d0974faeb2d20b201dabb86073
Matheus Pedroso <matheus_pedroso2012@hotmail.com>
2025-07-09 01:22:36 -0300

    Add Implementation Interfaces in the Infrastructure

---
+++ Catalog.Infrastructure/Identity/AuthenticateServices.cs
@@ -0,0 +1,34 @@
+﻿using Catalog.Domain.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace Catalog.Infrastructure.Identity;
+
+public class AuthenticateServices(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : IAuthenticate
+{
+    public async Task<bool> Authenticate(string email, string password)
+    {
+        var result = await signInManager.PasswordSignInAsync(email, password, false, lockoutOnFailure: false);
+
+        return result.Succeeded;
+
+    }
+    public async Task<bool> Register(string email, string password)
+    {
+        var applicationUser = new ApplicationUser
+        {
+            UserName = email,
+            Email = email
+        };
+
+        var result = await userManager.CreateAsync(applicationUser, password);
+
+        if (result.Succeeded) 
+            await signInManager.SignInAsync(applicationUser, isPersistent: false);
+
+        return result.Succeeded;
+    }
+    public async Task Logout()
+    {
+        await signInManager.SignOutAsync();
+    }
+}
